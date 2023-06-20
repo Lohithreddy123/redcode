8432f790a139a627f4f2822e865994fa36502e41
Lohithreddy123 <131978791+Lohithreddy123@users.noreply.github.com>
2023-06-20 11:39:54 +0530

    Add files via upload

---
+++ myprg.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "Hi there"
+sleep 5
+echo "Hi there"
+sleep 5
+echo "hi there"
+sleep 5
+echo "hi there"
+sleep 5
+echo "Hi there"
+sleep 5
+echo "Hi there"
+sleep 5
+echo "hi there"
+sleep 10
+echo "hi there"
+sleep 10
+echo "hi there"
+sleep 10
+sleep 5
+echo "hi there"
+sleep 10
+sleep 5
+echo "hi there"
+sleep 10
+sleep 5
+echo "hi there"
+sleep 10
+sleep 5
+echo "hi there"
+sleep 10
+echo "hi there"
+sleep 10
+echo "hi there"
+sleep 10
+
